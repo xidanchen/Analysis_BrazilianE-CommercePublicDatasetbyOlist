12e1530c4d52431a3a05295b204b5a964b90f35a
xidan chen <xidan.chen@gmail.com>
2021-08-06 11:41:03 +0800

    commit2
    add tableau visualization data

---
+++ simpleAnalysis.sql
@@ -0,0 +1,34 @@
+USE olistpublic;
+
+## An order might have multiple items.
+## Each item might be fulfilled by a distinct seller.
+## All text identifying stores and partners where replaced by the names of Game of Thrones great houses.
+## customerunique_id customers that made repurchases at the store
+## each order is assigned to a unique customerid
+# every purchase has a unique longitude and latitude？
+#customers made more than two purchases
+-- old customer
+SELECT
+COUNT(customer_id) AS purchases,
+customer_unique_id,
+customer_zip_code_prefix,
+customer_city,
+customer_state
+FROM
+customers c
+GROUP BY customer_unique_id
+HAVING COUNT(customer_id) >= 2
+;
+
+-- new customer
+SELECT
+COUNT(customer_id),
+customer_unique_id,
+customer_zip_code_prefix,
+customer_city,
+customer_state
+FROM
+customers
+GROUP BY customer_unique_id
+HAVING COUNT(customer_id) >= 2
+;
