fe1edd34ba3856510ff9ec598f5e61aaaff6f798
xidan chen <xidan.chen@gmail.com>
2022-10-12 14:32:28 +0200

    Update combinedMarketingFunnelDataAnalysis.sql
    updated landing page and marketing channel performance analysis

--- combinedMarketingFunnelDataAnalysis.sql
+++ combinedMarketingFunnelDataAnalysis.sql
@@ -74,9 +74,138 @@ where seller_id is not null
 group by landing_page_id
 order by count(seller_id) desc
 ;
+select landing_page_id, count(mql_id)
+from marketing_funnel
+group by landing_page_id
+order by count(mql_id) desc
+;
+select landing_page_id, count(mql_id) as num_mql, count(seller_id) as num_closed_deals,
+count(seller_id)/count(mql_id) as conv_rt
+from marketing_funnel
+group by landing_page_id
+order by conv_rt desc
+;
 
+/*
+1. '22c29808c4f815213303f8933030604c', and 'b76ef37428e6799c421989521c0e5077'
+are two most viewed landing_page, their conversion rate are 19.71% and 18.75% respectively
+2. there are a few landing pages having really high conversion rate, unfortunately there's no data about this
+so i am not able to inversigate further. 
+*/
+
+#### landing page by month
+/*
+1. two most visited landing pages' conversion rates significant increased from 2018 and were stable from 2018
+2. 22c29808c4f815213303f8933030604c had highest conversion rate on tuesday
+2. b76ef37428e6799c421989521c0e5077 had highest conversion rate on saturday
+*/
+select DATE_FORMAT(first_contact_date,'%Y%m') as contact_date,
+landing_page_id, count(mql_id) as num_mql, count(seller_id) as num_closed_deals,
+count(seller_id)/count(mql_id) as conv_rt
+from marketing_funnel
+where landing_page_id in ('22c29808c4f815213303f8933030604c', 'b76ef37428e6799c421989521c0e5077')
+group by landing_page_id, contact_date
+order by contact_date, conv_rt desc
+;
+
+#pivot table
+select DATE_FORMAT(first_contact_date,'%Y%m') as contact_date,
+count(case when landing_page_id = '22c29808c4f815213303f8933030604c' then seller_id else null end)/
+count(case when landing_page_id = '22c29808c4f815213303f8933030604c' then mql_id else null end) as c_conv,
+count(case when landing_page_id = 'b76ef37428e6799c421989521c0e5077' then seller_id else null end)/
+count(case when landing_page_id = 'b76ef37428e6799c421989521c0e5077' then mql_id else null end) as b_conv
+from marketing_funnel
+group by contact_date
+order by contact_date
+;
+
+select weekday(first_contact_date) as wkday_contact_date,
+count(case when landing_page_id = '22c29808c4f815213303f8933030604c' then seller_id else null end)/
+count(case when landing_page_id = '22c29808c4f815213303f8933030604c' then mql_id else null end) as c_conv,
+count(case when landing_page_id = 'b76ef37428e6799c421989521c0e5077' then seller_id else null end)/
+count(case when landing_page_id = 'b76ef37428e6799c421989521c0e5077' then mql_id else null end) as b_conv
+from marketing_funnel
+group by wkday_contact_date
+order by wkday_contact_date
+;
 
 
 
 ### marketing channel performance
+/* which marketing channels are driving most mql and closed deals
+ understanding differences in user characteristics and conversion performance across marketing channels
+ optimizing bids and allocating marketing spend across a multi-channel portfolio to achieve maximum performance
+ */
+select origin, count(mql_id) as num_mql, count(seller_id) as num_closed_deals,
+count(seller_id)/count(mql_id) as conv_rt
+from marketing_funnel
+group by origin
+order by conv_rt desc
+;
+/* 
+1. from the previous marketing_qualified_leads data analysis, we know organic_search, paid_search, and social
+are three major contributor of mql. Here paid_search performed better than organic_search in terms of conversion rate.
+social has a really low conversion rate comparing to those two. some unknow origins have really good conversion rate. It will
+be intersting to dig into and have better understanding of those traffic sources. 
+2. note paid_search is only slightly better than non_paid(organic_search, direct_traffic)
+3. organic_search performed as well as paid_search by month, it seems like the brand is doing well
+*/
+
+select DATE_FORMAT(first_contact_date,'%Y%m') as contact_date,
+origin, count(mql_id) as num_mql, count(seller_id) as num_closed_deals,
+count(seller_id)/count(mql_id) as conv_rt
+from marketing_funnel
+where origin in ('organic_search', 'social', 'paid_search')
+group by origin, contact_date
+order by contact_date, conv_rt desc
+;
+
+#pivot table
+select DATE_FORMAT(first_contact_date,'%Y%m') as contact_date,
+count(case when origin = 'organic_search' then seller_id else null end)/
+count(case when origin = 'organic_search' then mql_id else null end) as organic_search_conv,
+count(case when origin = 'social' then seller_id else null end)/
+count(case when origin = 'social' then mql_id else null end) as social_conv,
+count(case when origin = 'paid_search' then seller_id else null end)/
+count(case when origin = 'paid_search' then mql_id else null end) as paid_search_conv
+from marketing_funnel
+group by contact_date
+order by contact_date
+;
+
+#### is there weekday difference?
+/*
+1. organic_search and paid_search had highest conversion rate on saturday
+2. social had highest conversion rate on sunday
+*/
+select weekday(first_contact_date) as wkday_contact_date,
+count(case when origin = 'organic_search' then seller_id else null end) as organic_search_closed_deals,
+count(case when origin = 'social' then seller_id else null end) as social_closed_deals,
+count(case when origin = 'paid_search' then seller_id else null end) as paid_search_closed_deals,
+count(case when origin = 'organic_search' then mql_id else null end) as organic_search_mql,
+count(case when origin = 'social' then mql_id else null end) as social_mql,
+count(case when origin = 'paid_search' then mql_id else null end) as paid_search_mql,
+count(case when origin = 'organic_search' then seller_id else null end)/
+count(case when origin = 'organic_search' then mql_id else null end) as organic_search_conv,
+count(case when origin = 'social' then seller_id else null end)/
+count(case when origin = 'social' then mql_id else null end) as social_conv,
+count(case when origin = 'paid_search' then seller_id else null end)/
+count(case when origin = 'paid_search' then mql_id else null end) as paid_search_conv
+from marketing_funnel
+group by wkday_contact_date
+order by wkday_contact_date
+;
+
+### marketing channel by landing page
+select origin, landing_page_id, 
+count(mql_id) as num_mql, count(seller_id) as num_closed_deals,
+count(seller_id)/count(mql_id) as conv_rt
+from marketing_funnel
+where origin in ('organic_search', 'social', 'paid_search') and 
+landing_page_id in ('22c29808c4f815213303f8933030604c', 'b76ef37428e6799c421989521c0e5077')
+group by origin, landing_page_id
+order by conv_rt desc
+;
+
+
 
