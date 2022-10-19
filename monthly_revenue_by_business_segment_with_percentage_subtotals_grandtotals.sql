04c7b7a6e4ba56c532c3fb12529e9b0d266b4839
xidan chen <xidan.chen@gmail.com>
2022-10-19 12:23:40 +0200

    Update monthly_revenue_by_business_segment_with_percentage_subtotals_grandtotals.sql

--- monthly_revenue_by_business_segment_with_percentage_subtotals_grandtotals.sql
+++ monthly_revenue_by_business_segment_with_percentage_subtotals_grandtotals.sql
@@ -1,6 +1,6 @@
 ### Monthly Revenues by Business Segment (Revenue is calculated by summing up price)(with subtotals and grand totals)
 select A.*, 
-concat(round(revenue * 100/first_value(revenue) over w, 0), '%') as monthly_pct
+concat(round(revenue * 100/first_value(revenue) over w, 2), '%') as monthly_pct
 from
 (select if(grouping(date_format(order_purchase_timestamp, '%Y%m')), 'All Month', date_format(order_purchase_timestamp, '%Y%m')) as order_month, 
 if(grouping(business_segment), 'All Segments', business_segment) as business_segment,
