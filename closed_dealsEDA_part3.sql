04fb1c2bef56ea88a3225d832afa538106c5a463
xidan chen <xidan.chen@gmail.com>
2022-10-20 16:15:48 +0200

    Create closed_dealsEDA_part3.sql

---
+++ closed_dealsEDA_part3.sql
@@ -0,0 +1,8 @@
+### product category analysis
+select business_segment, product_category_name_english
+from marketing_orders
+;
+# 57 product category
+select distinct product_category_name_english
+from marketing_orders
+;
